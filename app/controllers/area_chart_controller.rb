65d9958f16047141bb14ce2ccdcce26093080de5
ishmael <ishmael@blackbalto.com>
2010-11-16 14:51:45 +0000

    area stacked chart

--- app/controllers/area_chart_controller.rb
+++ app/controllers/area_chart_controller.rb
@@ -101,12 +101,71 @@ class AreaChartController < ApplicationController
 				name: 'Joe',
 				data: [3, 4, 4, -2, 5]
 			}]
-			  
-
   })
   end
 
   def stacked_area
+   @chart = Highchart.area({
+    :chart => {
+			:renderTo => 'container',
+		},
+		:title => {
+			:text => 'Historic and Estimated Worldwide Population Growth by Region'
+		},
+		:subtitle => {
+			:text => 'Source: Wikipedia.org'
+		},
+		:x_axis => {
+			:categories => ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+			:tickmarkPlacement => 'on',
+			:title => {
+				:enabled => false
+			}
+		},
+		:y_axis => {
+			:title => {
+				:text => 'Billions'
+			},
+			:labels => {
+				:formatter => "function() {
+					return this.value / 1000;
+				}"
+			}
+		},
+		:tooltip => {
+			:formatter => "function() {
+				return ''+
+					 this.x +': '+ Highcharts.numberFormat(this.y, 0, ',') +' millions';
+			}"
+		},
+		:plotOptions => {
+			:area => {
+				:stacking => 'normal',
+				:lineColor => '#666666',
+				:lineWidth => 1,
+				:marker => {
+					:lineWidth => 1,
+					:lineColor => '#666666'
+				}
+			}
+		},
+		:series => [{
+			name: 'Asia',
+			data: [502, 635, 809, 947, 1402, 3634, 5268]
+		}, {
+			name: 'Africa',
+			data: [106, 107, 111, 133, 221, 767, 1766]
+		}, {
+			name: 'Europe',
+			data: [163, 203, 276, 408, 547, 729, 628]
+		}, {
+			name: 'America',
+			data: [18, 31, 54, 156, 339, 818, 1201]
+		}, {
+			name: 'Oceania',
+			data: [2, 2, 2, 6, 13, 30, 46]
+		}]
+		})
   end
 
   def percentage_area
