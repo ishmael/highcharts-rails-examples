977f9e10b095c4e77eb84f64391e19f3414b3f49
ishmael <ishmael@blackbalto.com>
2010-11-16 16:14:53 +0000

    column stack percentage chart

--- app/controllers/column_bar_chart_controller.rb
+++ app/controllers/column_bar_chart_controller.rb
@@ -266,6 +266,44 @@ class ColumnBarChartController < ApplicationController
   end
 
   def stack_perc_column
+    @chart = Highchart.column({
+        :chart => {
+    			:renderTo => 'container',
+    		},
+    		:title => {
+					:text => 'Stacked column chart'
+				},
+				:x_axis => {
+					:categories => ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+				},
+				:y_axis => {
+					:min => 0,
+					:title => {
+						:text => 'Total fruit consumption'
+					}
+				},
+				:tooltip => {
+					:formatter => "function() {
+						return ''+
+							 this.series.name +': '+ this.y +' ('+ Math.round(this.percentage) +'%)';
+					}"
+				},
+				:plotOptions => {
+					:column => {
+						:stacking => 'percent'
+					}
+				},
+			  :series => [{
+					name: 'John',
+					data: [5, 3, 4, 7, 2]
+				}, {
+					name: 'Jane',
+					data: [2, 2, 3, 2, 1]
+				}, {
+					name: 'Joe',
+					data: [3, 4, 4, 2, 5]
+				}]
+    		})
   end
 
   def column_rotated_labels
