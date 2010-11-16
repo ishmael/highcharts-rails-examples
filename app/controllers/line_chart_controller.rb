37896d9c0dc815fe105fc1a10eea32573d7b4aac
ishmael <ishmael@blackbalto.com>
2010-11-16 11:18:00 +0000

    inverted spline chart

--- app/controllers/line_chart_controller.rb
+++ app/controllers/line_chart_controller.rb
@@ -361,6 +361,69 @@ class LineChartController < ApplicationController
   end
 
   def spline_inverted
+    chart_data = [{
+				name: 'Temperature',
+				data: [[0, 15], [10, -50], [20, -56.5], [30, -46.5], [40, -22.1], 
+					[50, -2.5], [60, -27.7], [70, -55.7], [80, -76.5]]
+			}]
+		
+		tooltip_formatter = '
+      function() {return ""+this.x +" km: "+ this.y +"\u00B0C";}'
+      
+    @chart = Highchart.spline({
+      :chart => {
+				:renderTo => "container",	
+				:inverted => true,	
+				:width => 500,
+				:style => {
+				  :margin => '0 auto'
+				}
+			},
+			:title => {
+				:text => "Atmosphere Temperature by Altitude"
+			},
+			:subtitle => {
+				:text => 'According to the Standard Atmosphere Model'
+			},
+			:x_axis => {
+			  :reversed => false,
+        :title => {
+          :enabled =>true,
+          :text => 'Altitude',
+          :margin => 50
+        },
+        :labels => {
+          :formatter => "function() { return this.value + 'km';}"
+        },
+        :maxPadding => 0.05,
+        :showLastLabel => true
+			},
+			:y_axis => {
+				:title => {
+					:text => "Temperature"
+				},
+				:labels => {
+          :formatter => "function() { return this.value + '\u00B0';}"
+        },
+        :lineWidth => 2
+			},
+			:legend => {
+			  :enabled => false
+			},
+			:tooltip => {
+				:formatter => tooltip_formatter
+			},
+			:plotOptions => {
+				:spline => {
+					:marker => {
+						:enabled => true
+					}
+				}
+			},
+			:series => chart_data
+			  
+
+  })
   end
 
   def spline_symbols
